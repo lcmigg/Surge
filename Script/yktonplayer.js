f13dc5fd53060e0a226791675dbc853183c41235
lcmigg <lcmigg@gmail.com>
2020-04-20 11:56:51 +0800

    yktonplayer.js

--- Script/yktonplayer.js
+++ Script/yktonplayer.js
@@ -3,6 +3,8 @@
 使用方法：
 1.在[Script]分组下添加下面这行配置
 http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/115tonplayer.js
+
+^http:\/\/youku\.cdn4-okzy\.com\/share\/ https://raw.githubusercontent.com/lcmigg/Surge/master/Script/yktonplayer.js
 */
 $notification.post('播放地址提取成功, 长按(重按)通知查看', '',  'nplayer-' + $request.url);
 $done({}); 
