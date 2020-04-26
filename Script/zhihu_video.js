424717d29dff886311532668356f619bead110c9
lcmigg <lcmigg@gmail.com>
2020-04-26 11:56:46 +0800

    Update and rename zhihu_video to zhihu_video.js

---
+++ Script/zhihu_video.js
@@ -0,0 +1,7 @@
+let body = $response.body 
+body=JSON.parse(body)
+delete body.ad_info
+body=JSON.stringify(body)
+$done({body})
+
+// ^https:\/\/(api|lens|www)\.zhihu\.com\/api\/videos\/\d+\/recommend) url script-response-body https://raw.githubusercontent.com/lcmigg/Surge/master/Script/zhihu_video.js
