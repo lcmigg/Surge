73f62282cc35f8337a8722f50a03b1fcb0c3a9f9
lcmigg <lcmigg@gmail.com>
2020-04-28 14:43:06 +0800

    Update zhihu_video.js

--- Script/zhihu_video.js
+++ Script/zhihu_video.js
@@ -4,4 +4,4 @@ delete body.ad_info
 body=JSON.stringify(body)
 $done({body})
 
-// ^https:\/\/(api|lens|www)\.zhihu\.com\/api\/videos\/\d+\/recommend) url script-response-body https://raw.githubusercontent.com/lcmigg/Surge/master/Script/zhihu_video.js
+// ^https:\/\/(api|lens|www)\.zhihu\.com\/api\/videos\/\d+\/recommend url script-response-body https://raw.githubusercontent.com/lcmigg/Surge/master/Script/zhihu_video.js
