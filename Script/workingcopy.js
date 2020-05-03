5d80153313d14b5b62cdd32738204a4f6baa4d20
lcmigg <lcmigg@gmail.com>
2020-05-03 09:58:11 +0800

    workingcopy内购

---
+++ Script/workingcopy.js
@@ -0,0 +1,9 @@
+// @supported 6E2293897220
+$done({body: '{"student": true}'})
+
+/*
+利用 GitHub Education 解锁 Working Copy
+启用此脚本后再 Safari 打开 https://workingcopy.app/education/ 点击「tap here to access all pro features.」中的 here 跳转到 Working Copy，登录自己的 GitHub 账号即可解锁
+http-response ^https:\/\/education\.github\.com\/api\/user$ requires-body=1,max-size=0,script-path=scripts/WorkingCopy.js
+MitM = education.github.com
+*/
