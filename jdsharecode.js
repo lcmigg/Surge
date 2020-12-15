cc6f52fb34bebde6c05f148d660cd99e969bddac
lcmigg <lcmigg@gmail.com>
2020-12-15 18:56:24 +0800

    Update jdsharecode.js

--- jdsharecode.js
+++ jdsharecode.js
@@ -4,56 +4,45 @@
 * https://github.com/lxk0301/jd_scripts/tree/master
 * 每个月1号，10号，20号凌晨2点清理一次数据库，清理后需重新点击链接提交互助码。
 * 
-* 故选择：每月1，10，20号的早上9点发送提醒上车
-* 
-* ==========================Quantumultx=========================
+* ============Quantumultx===============
+*
 * [task_local]
-* #京东助力码上车
-* 0 0 9 1,10,20 * ? https://raw.githubusercontent.com/an0na/R/master/jd_share_code.js, tag=京东助力码上车, enabled=true
-* 
+* # 京东助力码上车
+* # 每月1，10，20号的凌晨2点5分运行
+* 0 5 2 1,10,20 * ? https://raw.githubusercontent.com/an0na/R/master/jd_share_code.js, tag=京东助力码上车, enabled=true
+*
 */
 const $ = new Env('京东助力码上车');
 
-//let allCode = "732c806d465d427aab0c948e2ef8de17@1a3547c6feb4423487d2f450adda3f35@f1d15dd3dfa745e293b2343e85f0e065&mlrdw3aw26j3xogldu3rljozwt7b7lkvtlkywry@oikq73shoy33yjni64vntotbbm@4npkonnsy7xi3sny36skom3edho6thyj4hexnvy&MTAxODc2NTEzOTAwMDAwMDAyODExMjU5Nw==@MTAxODc2NTE0NzAwMDAwMDAzMDkxNjE0NQ==@MTE1NDQ5MzYwMDAwMDAwMzgyODU4MjE=";
-
-//【自定义的地方】请按照水果、种豆、宠物的顺序添加自己的助力码
-//例子：A@A1@A2&B@B1@B2&C@C1@C2   每种活动的多个账号助力码使用@连接,活动和活动之间使用&连接
-let allCode = "";
-
-//农场
-let farmArr = ['b35c3b140de049a0973c0bd315bc4510','f15305d300d94803842191c91dacf576'];
-//种豆
-let beanArr = ['gkk4336xszaegeuoy5ggnx3nesrpbavnyhehdci','nuxd2i4axqw55dcsmp7jj6whp4'];
-//萌宠
-let petArr = ['MTE1NDAxNzgwMDAwMDAwMzQxMjA5MTU=','MTAxODc2NTEzOTAwMDAwMDAyODk4ODY4MQ=='];
-//东东工厂
-let ddfactoryArr = ['P04z54XCjVWnYaS5mlbVWL43H9LkQ','P04z54XCjVWnYaS5m9cZwmKoiEC7Ka9CNnojw'];
-//京喜工厂
-let jxfactoryArr = ['RaaxfhOhJfAsKpJg7xwfLQ==','tvwIEw0Hl-evGtNOU5yerA=='];
-
-if(allCode){
-  let array = allCode.split('&');
-  farmArr = array[0].split('@');
-  beanArr = array[1].split('@');
-  petArr = array[2].split('@');
-  ddfactoryArr = [];
-  jxfactoryArr = [];
-}
-
-let farmApi = "http://api.turinglabs.net/api/v1/jd/farm/create/互助码/";
-let beanApi = "http://api.turinglabs.net/api/v1/jd/bean/create/互助码/";
-let petApi = "http://api.turinglabs.net/api/v1/jd/pet/create/互助码/";
-let ddfactoryApi = "http://api.turinglabs.net/api/v1/jd/ddfactory/create/互助码/";
-let jxfactoryApi = "http://api.turinglabs.net/api/v1/jd/jxfactory/create/互助码/";
+// 依次是水果农场、种豆、萌宠、东东工厂、京喜工厂
+let shareCodes = [
+    'f15305d300d94803842191c91dacf576@b35c3b140de049a0973c0bd315bc4510',
+    'nuxd2i4axqw55dcsmp7jj6whp4@gkk4336xszaegeuoy5ggnx3nesrpbavnyhehdci',
+    'MTAxODc2NTEzOTAwMDAwMDAyODk4ODY4MQ==@MTE1NDAxNzgwMDAwMDAwMzQxMjA5MTU=',
+    'P04z54XCjVWnYaS5mlbVWL43H9LkQ@P04z54XCjVWnYaS5m9cZwmKoiEC7Ka9CNnojw',
+    'RaaxfhOhJfAsKpJg7xwfLQ==@tvwIEw0Hl-evGtNOU5yerA=='
+];
 
 !(async () => {
-  for (let i = 0; i < 3; i++) {
-      $.msg(`【`+ getName(i) + `】农场水果-助力码上车`, ``, ``, {"open-url": farmApi.replace("互助码", farmArr[i])});
-      $.msg(`【`+ getName(i) + `】种豆得豆-助力码上车`, ``, ``, {"open-url": beanApi.replace("互助码", beanArr[i])});
-      $.msg(`【`+ getName(i) + `】萌宠-助力码上车`, ``, ``, {"open-url": petApi.replace("互助码", petArr[i])});
-      $.msg(`【`+ getName(i) + `】东东工厂-助力码上车`, ``, ``, {"open-url": ddfactoryApi.replace("互助码", ddfactoryArr[i])});
-      $.msg(`【`+ getName(i) + `】京喜工厂-助力码上车`, ``, ``, {"open-url": jxfactoryApi.replace("互助码", xfactoryArr[i])});
-  }
+   for (let i = 0; i < shareCodes.length; i++) {
+        for (let j = 0; j < shareCodes[i].split('@').length; j++) {
+            var code = shareCodes[i].split('@')[j];
+
+            var userName = getUserName(j);
+            var reqUrl = getReqUrl(code, i);
+            var activityName = getActivityName(i);
+
+            console.log(`\n 账号：`+ userName + `,活动：`+ activityName + `,请求地址：`+ reqUrl);
+
+            await goCar(reqUrl);
+            
+            $.msg(`【账号` + userName +`】`+ activityName, `返回码：${$.code}, 返回信息：${$.message}`, ``, {"open-url": reqUrl });
+
+            sleep(1000).then(() => {
+                console.log(`休息1s`);
+            });
+        }
+   }
 })().catch((e) => {
   $.log('', `异常!`, '')
 })
@@ -61,14 +50,76 @@ let jxfactoryApi = "http://api.turinglabs.net/api/v1/jd/jxfactory/create/互助�
   $.done();
 })
 
-function getName(i) {
-    let userName = "";
-    if(i == 0){
-      userName = "lcmig";
-    }else if(i == 1){
-      userName = "sxmei";
-    }
-    return userName;
+function getUserName(j) {
+    return "用户" + (j+1);
 }
+
+//上车
+function goCar(reqUrl) {
+  return new Promise(resolve => {
+    $.get({url: reqUrl}, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            
+            $.code = data['code'];
+            $.message = data['message'];
+
+            console.log(`data: ${JSON.stringify(data)}`);
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function getReqUrl(code, i) {
+    var reqUrl = "";
+    if(i == 0){
+      var url = "http://api.turinglabs.net/api/v1/jd/farm/create/互助码/";
+      reqUrl = url.replace("互助码", code);
+    }else if(i == 1){
+      var url = "http://api.turinglabs.net/api/v1/jd/bean/create/互助码/";
+      reqUrl = url.replace("互助码", code);
+    }else if(i == 2){
+      var url = "http://api.turinglabs.net/api/v1/jd/pet/create/互助码/";
+      reqUrl = url.replace("互助码", code);
+    }else if(i == 3){
+     var url = "http://api.turinglabs.net/api/v1/jd/ddfactory/create/互助码/";
+     reqUrl = url.replace("互助码", code);
+    }else if(i == 4){
+     var url = "http://api.turinglabs.net/api/v1/jd/jxfactory/create/互助码/";
+     reqUrl = url.replace("互助码", code);
+    }
+    return reqUrl;
+}
+
+function getActivityName(i) {
+    var activityName = "";
+    if(i == 0){
+      activityName = "农场水果-助力码上车";
+    }else if(i == 1){
+      activityName = "种豆得豆-助力码上车";
+    }else if(i == 2){
+      activityName = "萌宠-助力码上车";
+    }else if(i == 3){
+      activityName = "东东工厂-助力码上车";
+    }else if(i == 4){
+      activityName = "京喜工厂-助力码上车";
+    }
+    return activityName;
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 // prettier-ignore
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
