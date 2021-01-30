3043a04b14a76e548d8c3daf7fbf51b02be38f14
lcmigg <lcmigg@gmail.com>
2021-01-30 22:10:31 +0800

    Update IP_bili_cn.js

--- Script/IP_bili_cn.js
+++ Script/IP_bili_cn.js
@@ -52,7 +52,8 @@ emoji=City_ValidCheck(obj['province']) == "香港"? "🇭🇰️":emoji
 emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
 emoji=City_ValidCheck(obj['province']) == "台湾"? "️️🇹🇼":emoji
 //var title =  emoji +'『'+ City_ValidCheck(obj['province'])+'』';//+Area_check(obj['country']);
-var title =  City_ValidCheck(obj['province']);
+var title =  City_ValidCheck(obj['province']) + "⚩"+ emoji;
+//var title =  City_ValidCheck(obj['province'])
 var subtitle =  ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
 var ip = obj['addr']; 
 var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
