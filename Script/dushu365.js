27261440cacaecd99c1184db9a2ca976595816c3
lcmigg <lcmigg@gmail.com>
2021-08-07 11:39:59 +0800

    Update dushu365.js

--- Script/dushu365.js
+++ Script/dushu365.js
@@ -34,18 +34,20 @@ const appUser = '/user-orchestration/userWeb/appUser/v100/getUserByToken'
 
 if (url.indexOf(vip) != -1) {
   obj.data.userStatus = 3;
-  if (obj.data.uid == 6018290) {
-    obj.data.username = '隔壁老王'
+  if (obj.data.uid == 241034039) {
+    obj.data.username = 'lcmig'
   }
-  obj.data.expire_time = 2012745599000
+  //obj.data.expire_time = 2012745599000 结束时间
+  obj.data.expire_time = 1754537397000
   obj.data.isTrial = false
   obj.data.userRoleCode = '11'
-  obj.data.vipBeginTime = 1523980800000
+  //obj.data.vipBeginTime = 1523980800000
+  obj.data.vipBeginTime = 1628306997000
   body = JSON.stringify(obj);
 }
 
 if (url.indexOf(appUser) != -1) {
-  obj.data.expireTime = 2012745599000
+  obj.data.expireTime = 1754537397000
   obj.data.isTrial = false
   obj.data.userStatus = 3
   obj.data.userRoleCode = '11'
