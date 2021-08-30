48dbf3668af354128562ee95e25ec236841db62e
lcmigg <lcmigg@gmail.com>
2021-08-30 20:22:40 +0800

    Update ztjy.js

--- Script/ztjy.js
+++ Script/ztjy.js
@@ -1,6 +1,6 @@
 /*
 
-https://zths.szy.cn/api/(getExpInfo|getUserExpList) url script-response-body 
+https://zths.szy.cn/api/(getExpInfo|getUserExpList) url script-response-body https://raw.githubusercontent.com/lcmigg/Surge/master/Script/ztjy.js
 hostname = zths.szy.cn
 
 */
@@ -9,6 +9,8 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj.data.expirTime = 1882785945
-obj.date.userExpList.expId = 2
+obj.data.userExpList.expId = 2
+obj.data.userExpList.businessIsolId = 1
+obj.data.userExpList.globalIsolId = 1
 
 $done({body: JSON.stringify(obj)});
