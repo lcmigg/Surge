559c54fb976e25fb53fa348b00336996641c719b
lcmigg <lcmigg@gmail.com>
2021-08-30 20:33:27 +0800

    Update ztjy.js

--- Script/ztjy.js
+++ Script/ztjy.js
@@ -8,10 +8,10 @@ hostname = zths.szy.cn
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.data."expirTime" = 1882785945
-obj.data.userExpList.expId = 2
-obj.data.userExpList.businessIsolId = 2
-obj.data.userExpList.globalIsolId = 1
-obj.data.userExpList.levelId = 9
+obj.expirTime = 1882785945
+obj.userExpList.expId = 2
+obj.userExpList.businessIsolId = 2
+obj.userExpList.globalIsolId = 1
+obj.userExpList.levelId = 9
 
 $done({body: JSON.stringify(obj)});
