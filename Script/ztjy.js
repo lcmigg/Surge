c503e4e14b07bd65e0dff0b76a1694376e55b481
lcmigg <lcmigg@gmail.com>
2021-08-31 13:08:34 +0800

    Update ztjy.js

--- Script/ztjy.js
+++ Script/ztjy.js
@@ -7,16 +7,54 @@ hostname = zths.szy.cn
 let obj = JSON.parse($response.body);
 
 obj = {
-  "expirTime" : 1882785945
-  "userExpList" : [
+  "message" : "成功",
+  "returncode" : "10000",
+  "body" : {
+    "clazzExpList" : [
+
+    ],
+    "expList" : [
+      "7_33",
+      "15_53",
+      "1_6"
+    ],
+    "businessIsolList" : [
+
+    ],
+    "expirTime" : 1882785945,
+    "commonExpList" : [
+
+    ],
+    "studentExpList" : [
+
+    ],
+    "globalIsolList" : [
+
+    ],
+    "userExpList" : [
       {
-        "expId" : 2,
+        "expId" : 3,
         "expGroupId" : 6,
         "businessIsolId" : 1,
-        "globalIsolId" : 1,
+        "globalIsolId" : 0,
         "levelId" : 3
       }
     ],
+    "schoolExpList" : [
+      {
+        "expId" : 7,
+        "expGroupId" : 33,
+        "levelId" : 5,
+        "globalIsolId" : 0
+      },
+      {
+        "expId" : 15,
+        "expGroupId" : 53,
+        "levelId" : 14,
+        "globalIsolId" : 0
+      }
+    ]
+  }
 }
 
 $done({body: JSON.stringify(obj)});
