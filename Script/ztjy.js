e12cfd747e4c3a3d786f490244fa9e992928012a
lcmigg <lcmigg@gmail.com>
2021-09-01 16:12:55 +0800

    Update ztjy.js

--- Script/ztjy.js
+++ Script/ztjy.js
@@ -11,16 +11,16 @@ let url = $request.url;
 let obj = JSON.parse($response.body);
 
 if (url.indexOf(path1) != -1) {
-    obj."expirTime" = 1882785945;
-    obj."userExpList"."expId" = 2
-    obj."userExpList"."expGroupId" = 6
-    obj."userExpList"."businessIsolId" = 1
-    obj."userExpList"."globalIsolId" = 1
-    obj."userExpList"."levelId" = 1
+    obj.expirTime = 1882785945;
+    obj.userExpList.expId = 2
+    obj.userExpList.expGroupId = 6
+    obj.userExpList.businessIsolId = 1
+    obj.userExpList.globalIsolId = 1
+    obj.userExpList".levelId = 1
 }
 
 if (url.indexOf(path2) != -1) {
-    obj."expirTime" = 1882785945;
+    obj.expirTime = 1882785945;
 }
 
 $done({body: JSON.stringify(obj)}); 
