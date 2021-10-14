90b1dfcfaf33ca8562d4caebd32c0ca0bfaa938d
lcmigg <lcmigg@gmail.com>
2021-10-14 20:10:51 +0800

    轻颜 蒸汽 documents

--- Script/documents.js
+++ Script/documents.js
@@ -1,20 +1,48 @@
-/*
-#document
-^https:\/\/license\.pdfexpert\.com\/api\/.*\/documents\/subscription\/(refresh$|check$) url script-response-body JS/documents.js
-license.pdfexpert.com
-*/
 
 var obj= {
-  "originalTransactionId" : "20000625420102",
-  "subscriptionState" : "trial",
-  "isInGracePeriod" : false,
-  "subscriptionExpirationDate" : "17:48 25/11/2099",
-  "isDocuments6User" : true,
-  "isEligibleForIntroPeriod" : false,
-  "subscriptionAutoRenewStatus" : "autoRenewOff",
-  "subscriptionReceiptId" : "1530908572000"
+  "productId": "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user",
+  "subscriptionExpirationIntent": "userCancelled",
+  "receiptStatus": "ok",
+  "subscriptionExpirationDate": "17:48 25/11/2099",
+  "inAppStates": [
+    {
+      "productId": "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user",
+      "subscriptionExpirationIntent": "userCancelled",
+      "receiptStatus": "ok",
+      "subscriptionExpirationDate": "17:48 25/11/2019",
+      "isEligibleForIntroPeriod": false,
+      "originalTransactionId": "710000781084863",
+      "productName": "subscription",
+      "isInBillingRetryPeriod": false,
+      "type": "subscription",
+      "subscriptionState": "active",
+      "subscriptionAutoRenewStatus": "autoRenewOff",
+      "isInGracePeriod": false
+    },
+    {
+      "originalTransactionId": "0000",
+      "entitlements": [
+      ],
+      "type": "custom purchase",
+      "productId": "documents6-user"
+    }
+  ],
+  "isDocuments6User": true,
+  "isEligibleForIntroPeriod": false,
+  "originalTransactionId": "710000781084863",
+  "isEligibleFor": [
+    "winback"
+  ],
+  "isInBillingRetryPeriod": false,
+  "type": "subscription",
+  "inAppPurchased": [
+    "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user"
+  ],
+  "bundleId": "com.readdle.ReaddleDocsIPad",
+  "receiptId": 1604898429000,
+  "chargingPlatform": "iOS AppStore",
+  "subscriptionState": "active",
+  "subscriptionAutoRenewStatus": "autoRenewOff",
+  "isInGracePeriod": false
 };
-
 $done({body: JSON.stringify(obj)});
-
-// Descriptions
