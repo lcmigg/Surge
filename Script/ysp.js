ce89564a4a2d76659c572d338a4298d3891ab47c
lcmigg <lcmigg@gmail.com>
2024-08-11 18:31:33 +0800

    Create ysp.js

---
+++ Script/ysp.js
@@ -0,0 +1,18 @@
+/*
+20240805.18.57
+[rewrite_local]
+# 广告
+^https?:\/\/cdn\.cmgadx\.com\/sdk\/pool\/.+\.json url reject-dict
+
+#会员
+^http:\/\/(liveinfo|bkliveinfo|playvv)\.ysp\.cctv\.cn\/(playvinfo\?.+|.*) url script-request-header ysp.js
+
+[mitm]
+hostname = *.ysp.cctv.cn, cdn.cmgadx.com
+*/
+
+let headers = $request.headers;
+headers.Cookie = "_vide9";
+$done({
+  "headers": headers
+});
