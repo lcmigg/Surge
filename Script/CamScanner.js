e44055c95d9d1fb88c3df4efa094150b5466858e
lcmigg <lcmigg@gmail.com>
2026-02-24 15:17:31 +0800

    Update CamScanner.js

--- Script/CamScanner.js
+++ Script/CamScanner.js
@@ -1,87 +1,41 @@
-/*************************************
-项目名称：扫描全能王 解锁黄金会员
-下载地址：https://t.cn/A6ouHe4B
-脚本作者：chxm1023
-使用说明：解锁部分功能，使用前先开脚本。
-使用声明：⚠️仅供参考，🈲转载与售卖！
-**************************************
+/*
+ * CamScanner 解锁会员
+ * 版本: 1.0.0
+ * 适用: Loon
+ */
 
-[rewrite_local]
+const url = $request.url;
+const method = $request.method;
 
-^https:\/\/.*\.(intsig\.net|camscanner\.com) url script-response-body https://raw.githubusercontent.com/lcmigg/Surge/refs/heads/master/Script/CamScanner.js
-
-[mitm]
-
-hostname = *.camscanner.com, *.intsig.net
-
-*************************************/
-var chxm1023 = {};
-try {
-    chxm1023 = JSON.parse($response.body);
-} catch (e) {
-    console.log("原始响应非JSON格式，跳过处理");
+if (url.includes('/purchase/cs/query_property')) {
+  // 获取原始响应
+  let body = $response.body;
+  
+  if (body) {
+    try {
+      // 解析原始JSON
+      let obj = JSON.parse(body);
+      
+      // 修改为会员数据
+      obj = {
+        "data": {
+          "psnl_vip_property": {
+            "expiry": 4102415999,
+            "svip": 1,
+            "nxt_renew_tm": 4102415999
+          }
+        }
+      };
+      
+      // 返回修改后的body
+      $done({body: JSON.stringify(obj)});
+    } catch (e) {
+      console.log("解析错误: " + e);
+      $done({body});
+    }
+  } else {
     $done({});
+  }
+} else {
+  $done({});
 }
-
-const vipa = '/purchase/cs/query_property';
-const vipb = '/queryProperty';
-const tqzx = '/getPrivilegeItem';
-
-const vip = {
-    "group1_paid": 1,
-    "ms_first_pay": 0,
-    "vip_type": "svip",
-    "auto_renewal": true,
-    "in_trial": 1,
-    "members_page": 0,
-    "pc_vip": 1,
-    "renew_type": "year",
-    "renew_method": "appstore",
-    "ys_first_pay": 0,
-    "initial_tm": "4092599349",
-    "product_id": "com.intsig.camscanner.premiums.oneyear.autorenewable.free.test1",
-    "vip_level_info": {
-        "score": 0,
-        "level": 0,
-        "next_score": 1,
-        "start_score": 0,
-        "create_time": 0
-    },
-    "nxt_renew_tm": "4092599349",
-    "last_payment_method": "appstore",
-    "grade": 2,
-    "svip": 1,
-    "expiry": 4092599349,
-    "pending": 0,
-    "level_info": {
-        "level": 1,
-        "end_days": 30,
-        "days": 1
-    },
-    "inherited_flag": 0,
-    "group2_paid": 0
-};
-
-if ($request.url.indexOf(vipa) !== -1) {
-    if (chxm1023.data) {
-        chxm1023.data["psnl_vip_property"] = vip;
-        chxm1023.data["fax_balance"] = "99999";
-        // ...保持其他属性设置不变
-    }
-}
-
-if ($request.url.indexOf(vipb) !== -1) {
-    if (chxm1023.data && chxm1023.data.ar_property) {
-        chxm1023.data.ar_property["psnl_vip_property"] = vip;
-    }
-}
-
-if ($request.url.indexOf(tqzx) !== -1) {
-    if (chxm1023.data) {
-        chxm1023.data.data = {
-            // ...保持原有数据结构不变
-        };
-    }
-}
-
-$done({ body: JSON.stringify(chxm1023) });
